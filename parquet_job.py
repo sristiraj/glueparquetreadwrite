829f81639e7503347b7691605c2016ef6f90991c
sristiraj <sristiraj.a@gmail.com>
2021-11-02 22:32:47 +0530

    Update parquet_job.py

--- parquet_job.py
+++ parquet_job.py
@@ -5,7 +5,7 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
-args = getResolvedOptions(sys.argv, ["JOB_NAME","INPUT_DATA_PATH","OUTPUT_DATA_PATH"])
+args = getResolvedOptions(sys.argv, ["JOB_NAME","INPUT_DATA_PATH","OUTPUT_DATA_PATH","GLUE_DB_NAME","GLUE_TABLE_NAME"])
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
@@ -14,9 +14,9 @@ job.init(args["JOB_NAME"], args)
 
 input_data = args["INPUT_DATA_PATH"]
 output_data = args["OUTPUT_DATA_PATH"]
+glue_db = args["GLUE_DB_NAME"]
+glue_table = args["GLUE_TABLE_NAME"]
 
-print(input_data)
-print(output_data)
 # Script generated for node S3 bucket
 S3bucket_node1 = glueContext.create_dynamic_frame.from_options(
     format_options={},
@@ -41,7 +41,7 @@ S3bucket_node3 = glueContext.getSink(
 )
 
 S3bucket_node3.setCatalogInfo(
-    catalogDatabase="db1", catalogTableName="parquet_job_parquet"
+    catalogDatabase=glue_db, catalogTableName=glue_table
 )
 
 S3bucket_node3.setFormat("glueparquet")
